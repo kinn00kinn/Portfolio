a65928d9e001b205460058b0a105cfa3de6877b0
kinn00kinn <haruki1009kk@gmail.com>
2025-12-03 11:23:07 +0900

    tailwindcssno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
